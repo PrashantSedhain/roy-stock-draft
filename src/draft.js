6003e66876acb14712ab79ba3c43185c1304b211
Prashant Sedhain <prashantased@gmail.com>
2026-07-20 16:15:53 -0500

    Rename Emanuel to Manuel

--- src/draft.js
+++ src/draft.js
@@ -6,7 +6,7 @@ export const SEASON = Object.freeze({
 
 export const DRAFT = Object.freeze([
   { name: "Nate", short: "SKHY", picks: ["NBIS", "ZETA", "GOOG", "AVGO", "MRVL", "AMD", "MU", "CRDO", "LITE", "CEG", "TSM", "SHOP", "SKHY", "NUAI", "BYND"] },
-  { name: "Emanuel", short: "LMND", picks: ["CRWV", "ASTS", "MELI", "WIX", "HROW", "FPS", "SEDG", "ACMR", "MP", "CLPT", "ELVA", "OMDA", "LMND", "NRXS", "SPCX"] },
+  { name: "Manuel", short: "LMND", picks: ["CRWV", "ASTS", "MELI", "WIX", "HROW", "FPS", "SEDG", "ACMR", "MP", "CLPT", "ELVA", "OMDA", "LMND", "NRXS", "SPCX"] },
   { name: "Dr. Roy", short: "AXON", picks: ["META", "NVDA", "MSFT", "ORCL", "APP", "GLW", "AAOI", "NOK", "AMKR", "AEHR", "VG", "NVTS", "AXON", "AMPG", "BMBL"] },
   { name: "Neil", short: "ELVA", picks: ["SOFI", "DLO", "NU", "RDDT", "AMZN", "UBER", "PENG", "INTC", "NFLX", "VOYG", "AXON", "LMND", "ELVA", "OWLT", "OPEN"] },
   { name: "Tevis", short: "PENG", picks: ["HOOD", "RKLB", "NOW", "PLTR", "BE", "SNDK", "TE", "FIGR", "SKHY", "SMCI", "ONDS", "IREN", "PENG", "PDYN", "POET"] }
